ac692f3cdd32f007b0f46702a39e2b435b82d600
TheBigBlackBoy <126817177+TheBigBlackBoy@users.noreply.github.com>
2024-03-20 00:52:13 -0400

    actualizarlo

---
+++ js/pass-page.js
@@ -0,0 +1,11 @@
+document.getElementById("inicio").addEventListener("click", function() {
+    var contenedores = document.querySelectorAll(".contenedor");
+    contenedores.forEach(function(contenedor) {
+      if (contenedor.style.display === "none") {
+        contenedor.style.display = "block";
+      } else {
+        contenedor.style.display = "none";
+      }
+    });
+  });
+  
